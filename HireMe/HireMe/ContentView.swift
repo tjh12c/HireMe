787afa792434f77314e14a77dddf26ad78540fb5
Tyler Hunnefeld <tyler.hunnefeld@mckesson.com>
2022-03-22 12:17:07 -0600

    Initial Commit

---
+++ HireMe/HireMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HireMe
+//
+//  Created by Tyler Hunnefeld on 3/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HireMe/HireMe/HireMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HireMeApp.swift
+//  HireMe
+//
+//  Created by Tyler Hunnefeld on 3/22/22.
+//
+
+import SwiftUI
+
+@main
+struct HireMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
